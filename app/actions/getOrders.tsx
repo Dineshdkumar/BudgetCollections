db09019ea5dc5d7b20742d1b5d8fd77ef7df7b26
Dinesh <dineshdkumar22@example.com>
2024-12-27 22:05:38 +0530

    d

--- app/actions/getOrders.tsx
+++ app/actions/getOrders.tsx
@@ -2,7 +2,7 @@ import prisma from "@/lib/prismadb";
 
 export const getOrders = async (user: any) => {
   if (!user || !user.id) {
-    throw new Error("User is not valid or user id is missing");
+    return []; // Return empty array if user is invalid
   }
 
   const orders = await prisma.order.findMany({
